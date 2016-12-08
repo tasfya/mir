84b460cc394193b08eb4c30b8c4df269edc07243
Hamza Betouar <hamza@betouar.com>
2016-12-08 16:49:03 +0000

    export all categories or one category

---
+++ test/Controller/CategoryControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hamza
+ * Date: 08/12/16
+ * Time: 04:42 م
+ */
+
+namespace Controller;
+
+
+use MirMigration\Controller\CategoryController;
+use MirMigration\Lib\AppFactory;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class CategoryControllerTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var CategoryController */
+    private $controller;
+
+    public function setUp(){
+        $factory = new AppFactory(Request::createFromGlobals());
+        $this->controller = $factory->getController(['controller' => 'category']);
+    }
+
+    public function testIndexAction(){
+        $response = $this->controller->indexAction();
+        $data = json_decode($response->getContent());
+        $this->assertEquals($data[0]->id,11);
+        $this->assertEquals($data[1]->id,12);
+    }
+
+    public function testViewAction(){
+        $response = $this->controller->viewAction(71);
+        $data = json_decode($response->getContent());
+        $this->assertEquals($data->place,29);
+    }
+
+}
