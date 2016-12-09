2a35d472ce3ccdf3d856db473aba69af3ba6798a
Hamza Betouar <hamza@betouar.com>
2016-12-09 17:24:28 +0000

    fix commit #d14a9ed

--- src/AutoLoader.php
+++ src/AutoLoader.php
@@ -7,10 +7,16 @@ class AutoLoader
 {
 
     public static function load(){
+        $loader = __DIR__.'/../vendor/autoload.php';
+        if (!file_exists($loader)) {
+            throw new \RuntimeException('Install dependencies.');
+        }
+        require_once $loader;
+
         $env = new Dotenv(__DIR__.'/../');
         $env->load();
 
-        return __DIR__.'/../vendor/autoload.php';
+        return $loader;
     }
 
 }
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -6,6 +6,6 @@ require __DIR__.'/../src/AutoLoader.php';
 $autoloadFile = AutoLoader::load();
 
 if (!file_exists($autoloadFile)) {
-    throw new RuntimeException('Install dependencies to run phpunit.');
+    throw new \RuntimeException('Install dependencies to run phpunit.');
 }
-require_once $autoloadFile;
+//require_once $autoloadFile;
